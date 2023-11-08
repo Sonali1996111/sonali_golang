40645c4537e4c0d479f2535343f34d7928b4f474
Sonali1996111 <sonalikshirsagar9058@gmail.com>
2023-11-08 12:39:07 +0530

    my work

---
+++ variables/bothdeclear.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var b int = 10 //both declearation,initilization  in one line.
+
+	fmt.Println("b", b)
+}
---
+++ variables/colanequal.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+//package level,we cant use colan equal to operater(:=)
+
+func main() {
+
+	c := 20
+
+	fmt.Println("c", c)
+
+}
