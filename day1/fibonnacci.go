ab7c06af065c6b83f761cf8d76b8c30e66310ca0
Sonali1996111 <sonalikshirsagar9058@gmail.com>
2023-11-28 08:44:38 +0530

    my day1 program

---
+++ day1/fibonnacci.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Fibonacci(n int) {
+
+	//F{n}=F{n-1}+F{n-2}
+	//where F{0}=0  and F{1}=1
+	var n3, n1, n2 int = 0, 0, 1
+	for i := 1; i <= n; i++ {
+		fmt.Println(n1)
+		n3 = n1 + n2
+		n1 = n2
+		n2 = n3
+	}
+}
+func main() {
+	Fibonacci(10)
+}
---
+++ day1/mathoperation.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number_1 = 10
+	var number_2 = 20
+	var sum = number_1 + number_2
+	var sub = number_1 - number_2
+	var multi = number_1 * number_2
+	var div = number_2 / number_1
+	var module = number_2 % number_1
+
+	fmt.Println(sub)
+	fmt.Println(sum)
+	fmt.Println(multi)
+	fmt.Println(div)
+	fmt.Println(module)
+
+}
